953ee293f0853531d5e3a40fc1e7eac7b4b7e13c
Connor Redmond <connred@icloud.com>
2017-04-11 11:25:44 -0500

    AP

---
+++ client/main.js
@@ -0,0 +1,161 @@
+$(document).ready(function () {
+    var socket = io.connect();
+    socket.on('Welcome', function (data) {
+        $('#log').append('<div><strong>' + data.text + '</strong></div>');
+    });
+    var profile; // google user profile
+    var authResponse; // google user auth response
+    function onSignIn(googleUser) {
+        profile = googleUser.getBasicProfile();
+        authResponse = googleUser.getAuthResponse();
+        var login = {
+            'id': profile.getId()
+            , 'name': profile.getName()
+            , 'givenName': profile.getGivenName()
+            , 'familyName': profile.getFamilyName()
+            , 'imageUrl': profile.getImageUrl()
+            , 'email': profile.getEmail()
+            , 'hostedDomain': googleUser.getHostedDomain()
+        }
+        post('/login', login);
+        $('.g-signin2').hide();
+        /*get('/addrooms', function (data) {
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].id && data[i].text) {
+                    $('#rooms').append('<div><strong><span>' + data[i].text + '</span></strong></div>');
+                }
+            }
+        });*/
+    }
+
+    function signOut() {
+        gapi.auth2.getAuthInstance().signOut();
+        $('.g-signin2').show();
+    }
+
+    function disconnect() {
+        gapi.auth2.getAuthInstance().disconnect();
+        $('.g-signin2').show();
+    }
+
+    function post(url, json, success, error) {
+        $.ajax({
+            url: route(url)
+            , method: 'POST'
+            , data: json
+            , headers: {
+                'Authorization': authResponse.id_token
+            }
+            , success: function () {
+                if (success) success();
+            }
+            , error: function () {
+                if (error) error();
+            }
+        });
+    }
+
+    function get(url, success, error) {
+        $.ajax({
+            url: route(url)
+            , method: 'GET'
+            , headers: {
+                'Authorization': authResponse.id_token
+            }
+            , success: function (data) {
+                if (success) success(data);
+            }
+            , error: function () {
+                if (error) error();
+            }
+        })
+    }
+
+    function put(url, json, success, error) {
+        $.ajax({
+            url: route(url)
+            , method: 'PUT'
+            , data: json
+            , headers: {
+                'Authorization': authResponse.id_token
+            }
+            , success: function (data) {
+                if (success) success(data);
+            }
+            , error: function () {
+                if (error) error();
+            }
+        })
+    }
+    var name;
+    $('#pass-enter').click(function () {
+        var password = $('#pass-input');
+        var realpassword = 'apcs';
+        var pwd = password.val().trim();
+        if (password.val().trim() === realpassword) {
+            $('.g-signin2').prop('hidden', false);
+            $('#user').prop('hidden', false);
+            $('#pass-input').prop('hidden', true);
+            $('#pass-enter').prop('hidden', true);
+            $('#span1').prop('hidden', true);
+        }
+        else {
+            alert('Incorrect Password');
+        }
+    });
+    $('#user-save').click(function () {
+        var username = $('#user-name');
+        var txt = username.val().trim();
+        console.log(txt);
+        if (txt.length > 0) {
+            name = txt;
+            username.prop('hidden', true);
+            $('#span2').hide();
+            $(this).hide();
+            $('#controls').show();
+            $('#show-button').prop('hidden, true');
+            $('#log').prop('hidden', false);
+            $('#message').prop('disabled', false);
+            $('#send').prop('disabled', false);
+            socket.emit('user', name);
+        }
+    });
+    $('#send').click(function () {
+        var input = $('#message');
+        var text = input.val().trim();
+        if (text.length > 0) {
+            socket.emit('message', text);
+            //console.log('Message sent by', name,":'",text,"'");
+        }
+        input.val('');
+    })
+    socket.on('message', function (data) {
+        $('#log').append('<div><strong>' + data.user + ': ' + data.message + '</strong></div>');
+        console.log('Message sent by', data.user, ":'", data.message, "'");
+    });
+    socket.on('otherUserConnect', function (data) {
+        $('#log').append('<div><strong>' + data + ' connected</strong></div>');
+    });
+    socket.on('otherUserDisconnect', function (data) {
+        $('#log').append('<div><strong>' + data + ' disconnected</strong></div>');
+    });
+    socket.on('displayname', function () {
+            $('#user').append('<span>' + 'You choose the name: ' + socket.user + '</span>');
+        })
+        // Pressing enter sends
+    $("#user-name").keyup(function (event) {
+        if (event.keyCode == 13) {
+            $("#user-save").click();
+        }
+    });
+    $("#pass-input").keyup(function (event) {
+        if (event.keyCode == 13) {
+            $("#pass-enter").click();
+        }
+    });
+    $("#message").keyup(function (event) {
+        if (event.keyCode == 13) {
+            $("#send").click();
+        }
+    });
+});
---
+++ server/server.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var jwt = require('jsonwebtoken'); 
+var fs = require('fs');
+var app = express();
+var moment = require('moment'); 
+var request = require('request');
+var jwkToPem = require('jwk-to-pem');
+var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser'); 
+var keyCache = {};
+const MONGO_URL = 'mongodb://localhost:27017/NCMongo';
+const CLIENT_ID = '100486091355-kggb1no0knblhq4oi7c8iov1kfnj7aut.apps.googleusercontent.com';
+var http = require('http').Server(app);
+var webroot = __dirname + '/../client/';
+app.use('/', express.static(webroot));
+Mongo.connect(MONGO_URL, function (err, db) {
+    // TODO: handle err
+    if (err) log('error?');
+    else log('good to go');
+    Mongo.ops = {};
+    Mongo.ops.find = function (collection, json, callback) {
+        db.collection(collection).find(json).toArray(function (err, docs) {
+            // TODO: handle err
+            if (callback) callback(err, docs);
+        });
+    };
+    Mongo.ops.findOne = function (collection, json, callback) {
+        db.collection(collection).findOne(json, function (err, doc) {
+            // TODO: handle err
+            if (callback) callback(err, doc);
+        });
+    };
+    Mongo.ops.insert = function (collection, json, callback) {
+        db.collection(collection).insert(json, function (err, result) {
+            // TODO: handle err
+            if (callback) callback(err, result);
+        });
+    };
+    Mongo.ops.upsert = function (collection, query, json, callback) {
+        db.collection(collection).updateOne(query, {
+            $set: json
+        }, {
+            upsert: true
+        }, function (err, result) {
+            // TODO: handle err
+            if (callback) callback(err, result);
+        });
+    };
+    Mongo.ops.updateOne = function (collection, query, json, callback) {
+        db.collection(collection).updateOne(query, {
+            $set: json
+        }, function (err, result) {
+            // TODO: handle err
+            if (callback) callback(err, result);
+        });
+    };
+});
+function allowCrossDomain(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
+    // end pre flights
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+    }
+    else {
+        next();
+    }
+}
+
+var server = http.listen(80, function() {
+    console.log('hosting from ' + webroot);
+    console.log('server listening on http://localhost/');
+});
+var users = [];
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+    var clientIp = socket.request.connection.remoteAddress;
+    console.log('socket connected from ' + clientIp);
+    socket.emit('Welcome', {
+        text: "Chat here:"
+    });
+    socket.on('user', function (name) {
+        console.log(username + ' connected');
+        users.push(username);
+        socket.user = username;
+        console.log('users : ' + users.length);
+        socket.broadcast.emit('otherUserConnect', socket.user);
+    });
+    socket.on('disconnect', function () {
+        if (!socket.user) {
+            return;
+        }
+        if (users.indexOf(socket.user) > -1) {
+            console.log(socket.user + ' disconnected');
+            users.splice(users.indexOf(socket.user), 1);
+            socket.broadcast.emit('otherUserDisconnect', socket.user);
+        }
+    });
+    socket.on('message', function (data) {
+        io.sockets.emit('message', {
+            user: socket.user
+            , message: data
+        })
+    });
+    //
+    
+});
