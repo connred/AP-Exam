835fa4cc8ba98a79c8bd3bc9c358eba4f29e34d4
Connor Redmond <connred@icloud.com>
2017-04-27 11:25:45 -0500

    Changes

--- server/server.js
+++ server/server.js
@@ -101,7 +101,7 @@ function cacheWellKnownKeys() {
     });
 }
 var usernames = {};
-var rooms = ['room1','room2','room3'];
+var rooms = ['Primary','Alternate'];
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
     var clientIp = socket.request.connection.remoteAddress;
@@ -118,7 +118,7 @@ io.sockets.on('connection', function (socket) {
 		// send client to room 1
 		socket.join('room1');
 		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+		socket.emit('updatechat', 'SERVER', 'you have connected to Primary');
 		// echo to room 1 that a person has connected to their room
 		socket.broadcast.to('room1').emit('updatechat', 'SERVER', login.name + ' has connected to this room');
 		socket.emit('updaterooms', rooms, 'room1');
@@ -134,19 +134,19 @@ io.sockets.on('connection', function (socket) {
 		socket.join(newroom);
 		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
 		// sent message to OLD room
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.user+' has left this room');
 		// update socket session room title
 		socket.room = newroom;
-		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.user+' has joined this room');
 		socket.emit('updaterooms', rooms, newroom);
 	});
     socket.on('disconnect', function(){
 		// remove the username from global usernames list
-		delete usernames[socket.username];
+		delete usernames[socket.user];
 		// update list of users in chat, client-side
 		io.sockets.emit('updateusers', usernames);
 		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+		socket.broadcast.emit('updatechat', 'SERVER', socket.user + ' has disconnected');
 		socket.leave(socket.room);
 	});
 
